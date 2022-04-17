2711cbbcd96f0a3e88884f1d249e7df5a8389afe
0finderfail <50328441+Shadowmaster346@users.noreply.github.com>
2022-04-17 13:21:39 +0300

    Add files via upload
    removed comments

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,9 @@
 import java.util.*
-
 fun main() {
-    println("Hello World!")
     val input = Scanner(System.`in`)
     val a = input.nextInt()
     var b = input.nextInt()
     val PI = 3.141592653589793
-//           3.141592653589793
-   // круто у нас туту ограничение в 15 символов после точки
-  //  println("test")
- //   val name = readLine()
-//    println("$name")
-    // Try adding program arguments at Run/Debug configuration
     println("Program arguments: $a < $b")
     println("Result: ${a < b}")
     println()
