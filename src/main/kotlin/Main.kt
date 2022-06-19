1bf690e928bd4ef235cf112f29804abe2886c5f0
0finderfail <50328441+Shadowmaster346@users.noreply.github.com>
2022-06-19 12:39:07 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,8 @@
-import java.util.*
-import test
 fun main() {
     val input = Scanner(System.`in`)
-    val a = input.nextInt()
-    var b = input.nextInt()
+    val a = 3754973572456736097275273527456634.294802944298742973
+    var b = 2302839582756834.230237529875892749028490184285425737
+    val PI = 3.141592653589793
     var classtest = test()
     println("Program arguments: $a < $b")
     println("Result: ${a < b}")
@@ -29,9 +28,9 @@ fun main() {
     println("Program arguments: $a * $b")
     println("Result: ${a * b}")
     println()
-    println("Program arguments: ${classtest.PI} < $b")
-    println("Result: ${classtest.PI < b}")
+    println("Program arguments: ${PI} < $b")
+    println("Result: ${PI < b}")
     println()
-    println("Program arguments: ${classtest.PI} > $b")
-    println("Result: ${classtest.PI > b}")
+    println("Program arguments: ${PI} > $b")
+    println("Result: ${PI > b}")
 }
