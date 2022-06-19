c12bb49602a9f88adb0c4e26ff4e9d11fc1f6759
0finderfail <50328441+Shadowmaster346@users.noreply.github.com>
2022-06-19 12:41:10 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,7 @@
 fun main() {
-    val input = Scanner(System.`in`)
     val a = 3754973572456736097275273527456634.294802944298742973
     var b = 2302839582756834.230237529875892749028490184285425737
     val PI = 3.141592653589793
-    var classtest = test()
     println("Program arguments: $a < $b")
     println("Result: ${a < b}")
     println()
